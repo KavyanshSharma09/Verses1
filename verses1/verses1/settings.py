81e6c468bbfcb512855372e7ca892f4daf0d1a91
Kavyansh Sharma <112067780+KavyanshSharma09@users.noreply.github.com>
2025-10-03 23:27:34 +0530

    Update settings.py

--- verses1/verses1/settings.py
+++ verses1/verses1/settings.py
@@ -23,7 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-a672mx!_(+m)dlsu4@+4^6yhf^vh#zx*j8tb5$9&4=!!54*)l#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
+
 
 ALLOWED_HOSTS = [
     'verses1-4.onrender.com',   # your Render domain
